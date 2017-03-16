ead58864a63812ae10520ded1eab608f7b397a3d
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2017-03-16 22:20:51 +0100

    Added configs for i3, gtk3 and polybar

---
+++ .config/i3/lock.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+set -e
+ICON=$HOME/.config/i3/lock-screen.png
+TMPBG=/tmp/screen.png
+scrot /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -u -i $TMPBG
