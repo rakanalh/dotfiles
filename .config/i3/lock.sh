3c5bae632ff461b98f4f4d0902d0b72a5e44daca
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2017-10-02 20:42:15 +0200

    Check in latest config updates to my i3 environment

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,9 +1,25 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-set -e
-ICON=$HOME/.config/i3/lock-screen.png
-TMPBG=/tmp/screen.png
-scrot /tmp/screen.png
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -u -i $TMPBG
+pgrep i3lock || {
+    FILE=/tmp/lock$RANDOM.png
+    TEXT=/tmp/locktext$(whoami).png
+    LOCK=$HOME/.config/i3/lock-screen.png
+
+    scrot -z $FILE
+    i3lock -i $FILE
+
+    [ -f $TEXT ] || {
+        convert -size 2560x60 xc:black -font Hack-Regular -pointsize 26 -fill "#00ff00" -gravity center -annotate +0+0 "$(whoami)@$(hostname)" $TEXT
+        convert $TEXT -alpha set -channel A -evaluate set 75% $TEXT;
+    }
+
+    convert $FILE -blur 0x7 $FILE
+    convert $FILE $LOCK -gravity center -composite $FILE
+    convert $FILE $TEXT -gravity center -geometry +0+200 -composite $FILE
+
+    kill $(pgrep i3lock)
+
+    i3lock -i $FILE
+    rm $FILE
+    # xset dpms force off
+}
